c1b7c8d8b0afe568e7a9d7f2dbb4ed5ed950e869
sean <sean@antfarm.de>
2017-01-04 13:29:22 +0100

    Re-added method for matrix generation.

--- Emanon/main.swift
+++ Emanon/main.swift
@@ -64,61 +64,57 @@ func main() {
 main()
 
 
-//func main() {
+func generate(width: Int, height: Int) -> [[Double]] {
+
+    srand48(42)
+
+    let emanon = EmanonPolymorphism()
+
+    var values = [[Double]]()
+
+    var maxValue = DBL_MIN
+    var minValue = DBL_MAX
+
+    Time.printExecutionTime("eval: ") {
+
+        emanon.createExpression(depth: 5)
+
+        //print(emanon.expressionString)
+
+        for x in 0 ..< width {
+
+            var row = [Double]()
+
+            for y in 0 ..< height {
+
+                let value = emanon.evalExpression(x: Double(x), y: Double(y))
+
+                maxValue = max(value, maxValue)
+                minValue = min(value, minValue)
+
+                //print("\(value) | \(minValue) ... \(maxValue)")
+
+                row.append(value)
+            }
+
+            values.append(row)
+        }
+    }
+
+    var valuesNormalized = [[Double]]()
+
+    for row in values {
+        let rowNormalized = row.map { ($0 - minValue) / (maxValue - minValue)}
+        valuesNormalized.append(rowNormalized)
+    }
+
+//    print("\(minValue) ... \(maxValue)")
 //
-//    srand48(42)
-//
-//    let emanon = Emanon()
-//
-//    let sizeX = 100
-//    let sizeY = 100
-//
-//    for _ in 1...100 {
-//
-//        var values = [[Double]]()
-//
-//        var maxValue = DBL_MIN
-//        var minValue = DBL_MAX
-//
-//        Time.printExecutionTime("eval: ") {
-//
-//            emanon.createExpression(depth: 5)
-//
-//            //print(emanon.expressionString)
-//
-//            for x in 0 ..< sizeX {
-//
-//                var row = [Double]()
-//
-//                for y in 0 ..< sizeY {
-//
-//                    let value = emanon.evalExpression(x: Double(x), y: Double(y))
-//
-//                    maxValue = max(value, maxValue)
-//                    minValue = min(value, minValue)
-//
-//                    //print("\(value) | \(minValue) ... \(maxValue)")
-//
-//                    row.append(value)
-//                }
-//
-//                values.append(row)
-//            }
-//        }
-//
-////        var valuesNormalized = [[Double]]()
-////
-////        for row in values {
-////            let rowNormalized = row.map { ($0 - minValue) / (maxValue - minValue)}
-////            valuesNormalized.append(rowNormalized)
-////        }
-////
-////        print("\(minValue) ... \(maxValue)")
-////
-////        for rowNormalized in valuesNormalized {
-////            print("\(rowNormalized.min()!) ... \(rowNormalized.max()!)")
-////        }
-////
-////        print()
+//    for rowNormalized in valuesNormalized {
+//        print("\(rowNormalized.min()!) ... \(rowNormalized.max()!)")
 //    }
-//}
+//
+//    print()
+
+    return valuesNormalized
+}
